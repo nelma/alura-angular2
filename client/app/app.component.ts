9ee1e40348916182648922c2a128b7de917b575d
Nelma <nelmasb@gmail.com>
2017-07-26 07:24:20 -0300

    3 - Consumindo uma API

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
     moduleId: module.id, //Faz com que o componente procure o template relativo à sua pasta.
@@ -7,4 +8,21 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
+    fotos: Object[] = [];
+
+
+    /**
+     * @Inject(Http) é a injeção de dependencia na definicao de classe.
+     * @Inject(Http) http ou http: Http
+     */
+    constructor(@Inject(Http) http) {
+
+        http.get('v1/fotos')
+            .map(res => res.json())
+            .subscribe(
+                fotos => this.fotos = fotos, //toda arrow function compartilha o mesmo this léxico do escopo do pai
+                error => console.log(error) //caso ocorra erro, devemos logar
+            )
+
+    }
 }
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,9 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FotoModule } from './foto/foto.module';
 
+//importar módulo que já possui um proovider configurado
+import { HttpModule } from '@angular/http';
+
+//importando extensao map
+import 'rxjs/add/operator/map';
+
 //NgModule transforma a classe numa classe do Angular
 @NgModule({
-    imports: [ BrowserModule, FotoModule ], //injecao de dependência
+    imports: [ BrowserModule, FotoModule, HttpModule ], //injecao de dependência
     declarations: [ AppComponent ], //importar componentes do projeto. O que faz parte do Modulo
     bootstrap: [ AppComponent ] //O que sera inicializado primeiro
 
